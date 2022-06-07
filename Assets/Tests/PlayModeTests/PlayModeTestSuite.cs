be73b6ffbb33b08ec798a6cca487ee050f839ade
Emerson <emersonye@gmail.com>
2022-06-06 23:41:26 -0700

    Fool's mate unit test

--- Assets/Tests/PlayModeTests/PlayModeTestSuite.cs
+++ Assets/Tests/PlayModeTests/PlayModeTestSuite.cs
@@ -25,6 +25,30 @@ public class PlayModeTestSuite
 
         Assert.NotNull(board.GetPieceOnSquare(new Vector2Int(0, 3)));
     }
+
+    [UnityTest]
+    public IEnumerator FoolsMate()
+    {
+        SceneManager.LoadScene("SampleScene");
+        yield return null;
+        boardInputHandler = GameObject.FindObjectOfType<BoardInputHandler>();
+        board = GameObject.FindObjectOfType<Board>();
+
+        // 1. f3, e6
+        ClickOnCoords(5, 1);
+        ClickOnCoords(5, 2);
+        ClickOnCoords(4, 6);
+        ClickOnCoords(4, 5);
+        // 2. g4, Qh4#
+        ClickOnCoords(6, 1);
+        ClickOnCoords(6, 3);
+        ClickOnCoords(3, 7);
+        ClickOnCoords(7, 3);
+        yield return null;
+
+        Assert.False(GameObject.FindObjectOfType<ChessGameController>().IsGameInProgress());
+    }
+
     private void ClickOnCoords(int x, int y)
     {
         boardInputHandler.ProcessInput(board.CalculatePositionFromCoords(new Vector2Int(x,y)), null, null);
